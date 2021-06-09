3c3096aa11ed8f2853e9ecdc54e3a0325a0a78ff
brkybzkrt <b9bozkurt959@gmail.com>
2021-06-09 18:10:50 +0300

    Updated Employer Controller

--- hrms/src/main/java/project/hrms/api/controllers/EmployersController.java
+++ hrms/src/main/java/project/hrms/api/controllers/EmployersController.java
@@ -1,20 +1,34 @@
 package project.hrms.api.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import project.hrms.business.abstracts.EmployerService;
 import project.hrms.core.utilities.results.DataResult;
-import project.hrms.core.utilities.results.Result;
+import project.hrms.core.utilities.results.ErrorDataResult;
+
 
 import project.hrms.entities.concretes.Employer;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/employers")
 public class EmployersController {
@@ -39,8 +53,26 @@ public class EmployersController {
 	}
 	
 	@PostMapping("/add")
-	public Result add(@RequestBody Employer employer){
+	public ResponseEntity<?> add(@Valid @RequestBody Employer employer){
 		
-		return this.employerService.add(employer);
+		return ResponseEntity.ok( this.employerService.add(employer));
+	}
+	
+	
+	
+	
+	
+		@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
+		
+		Map<String,String> validateErrors= new HashMap<String,String>();
+		for(FieldError fieldError:exceptions.getBindingResult().getFieldErrors()) {
+			
+			validateErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		ErrorDataResult<Object> errors= new ErrorDataResult<Object>(validateErrors,"Doğrulama hataları");
+		
+		return errors;
 	}
 }
