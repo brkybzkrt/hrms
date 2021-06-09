600997ac02e1d8eed93453648463edae192e1465
brkybzkrt <b9bozkurt959@gmail.com>
2021-06-09 18:15:20 +0300

    Updated JobAdvertisement Controller

--- hrms/src/main/java/project/hrms/api/controllers/JobAdvertisementsController.java
+++ hrms/src/main/java/project/hrms/api/controllers/JobAdvertisementsController.java
@@ -1,25 +1,37 @@
 package project.hrms.api.controllers;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import project.hrms.business.abstracts.JobAdvertisementService;
 
 import project.hrms.core.utilities.results.DataResult;
+import project.hrms.core.utilities.results.ErrorDataResult;
 import project.hrms.core.utilities.results.Result;
 import project.hrms.entities.concretes.JobAdvertisement;
 import project.hrms.entities.dtos.JobAdvertisementDto;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/jobAdvertisements/")
 public class JobAdvertisementsController {
@@ -39,9 +51,9 @@ public class JobAdvertisementsController {
 	} 
 	
 	@PostMapping("add")
- 	public Result add(@RequestBody JobAdvertisement jobAdvertisement) {
+ 	public ResponseEntity<?> add(@Valid @RequestBody JobAdvertisement jobAdvertisement) {
  		
- 		return this.jobAdvertisementService.add(jobAdvertisement);
+ 		return ResponseEntity.ok( this.jobAdvertisementService.add(jobAdvertisement));
  	}
 	
 	
@@ -89,6 +101,20 @@ public class JobAdvertisementsController {
 		return this.jobAdvertisementService.getByStatusOfActiveAndEmployer_CompanyName(true, employerName);
 		
 		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
+		
+		Map<String,String> validateErrors= new HashMap<String,String>();
+		for(FieldError fieldError:exceptions.getBindingResult().getFieldErrors()) {
+			
+			validateErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		ErrorDataResult<Object> errors= new ErrorDataResult<Object>(validateErrors,"Doğrulama hataları");
+		
+		return errors;
 	}
 	
 	
